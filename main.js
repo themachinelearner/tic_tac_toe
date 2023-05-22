eb65b9f5bb4f39aa946c4bce6bf6959cf0ce11df
Ben Smith <wiredcowboys@gmail.com>
2023-05-22 15:37:00 -0600

    Add display of score after each round and make it a 3 round game

--- main.js
+++ main.js
@@ -1,38 +1,59 @@
-// Take user input
-let userInput = prompt('Enter your move (Rock, Paper, or Scissors');
-
-// Validate user input
-if (!userInput) {
-    throw  new Error('Invalid user input');
+// Set initial game state
+game = {
+    score: {
+        player: 0,
+        computer: 0
+    },
+    roundsPlayed: 0
 }
 
-// Format user input
-let playerMove = userInput.slice(0,1).toUpperCase() + userInput.slice(1).toLowerCase();
-
-// Validate user input against valid choices
-if (!Object.values(VALID_MOVES).includes(playerMove)) {
-    throw  new Error('Invalid user input - user input does not match list of valid moves');
+while (game.score.player < 3 && game.score.computer < 3) {
+    playRound();
 }
 
-// Generate computer input
- let computerMove = generateRandomMove();
+console.log('Good game!');
 
-// Determine round outcome
-let currentRoundOutcome;
-console.log(`Player selected ${playerMove}, Computer Selected ${computerMove}.`);
-if (playerMove === computerMove) {
-    console.log('Tie!');
-} else if (playerMove.beats(computerMove)) {
-    console.log('Player wins!');
-} else if (computerMove.beats(playerMove)) {
-    console.log('Computer Wins!');
-} else throw new Error('Error determining winner of current round');
+function playRound() {
+    // Take user input
+    let userInput = prompt('Enter your move (Rock, Paper, or Scissors');
+
+    // Validate user input
+    if (!userInput) {
+        console.log('Invalid user input');
+        return;
+    }
+
+    // Format user input
+    let playerMove = userInput.slice(0,1).toUpperCase() + userInput.slice(1).toLowerCase();
+
+    // Validate user input against valid choices
+    if (!Object.values(VALID_MOVES).includes(playerMove)) {
+        console.log('Invalid move by player');
+        return;
+    }
+
+    // Generate computer input
+    let computerMove = generateRandomMove();
+
+    // Iterate rounds played
+    game.roundsPlayed = game.roundsPlayed + 1;
+
+    // Give player summary of moves
+    console.log(`Round ${game.roundsPlayed}: Player selected ${playerMove}, Computer Selected ${computerMove}.`);
+
+    // Take acion based on current round outcome
+    if (playerMove === computerMove) {
+        console.log('Tie!');
+    } else if (playerMove.beats(computerMove)) {
+        console.log('Player wins!');
+        game.score.player = game.score.player + 1;
+    } else if (computerMove.beats(playerMove)) {
+        console.log('Computer Wins!');
+        game.score.computer = game.score.computer + 1;
+    } else throw new Error('Error determining winner of current round');
 
 
-// Present outcome to user
+    // Present outcome to user
+    console.table(game.score);
+}
 
-
-
-// Update score
-
-// Begin a new round or present final scores
